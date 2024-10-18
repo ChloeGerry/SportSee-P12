7d7d9ba1cf033cf2479ebded25661d64a8359193
Chloé Gerry <cgerry@corp.free.fr>
2024-10-18 11:05:30 +0200

    refactor: rename app file by dashboard

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <>
-      <h1 className="text-5xl text-center">SportSee</h1>
-    </>
-  );
-};
-
-export default App;
---
+++ src/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+import Header from "./components/layout/Header";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
+};
+
+export default Dashboard;
