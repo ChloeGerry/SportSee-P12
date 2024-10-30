f7003fd61f196623e8d6d69c19143e012974a061
Chloé Gerry <cgerry@corp.free.fr>
2024-10-30 10:52:36 +0100

    chore: change error page message with more proper message

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -4,7 +4,7 @@ const Error = () => {
   return (
     <main className="flex flex-col justify-center items-center gap-4 h-screen">
       <h1 className="text-red text-9xl">404</h1>
-      <p className="text-lg">La page que vous demandez n'existe pas...</p>
+      <p className="text-lg">Oups... Il semble y avoir une erreur</p>
       <Link to="/" className="text-lg">
         Cliquer ici pour retourner dans votre espace personnel
       </Link>
