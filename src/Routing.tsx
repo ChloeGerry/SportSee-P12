a8290eaeed327b0ff89d3880c572714f8859dfdf
Chloé Gerry <cgerry@corp.free.fr>
2024-11-07 11:29:17 +0100

    fix: add more simple routing file to avoid redirection

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,17 +1,11 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Error from "./pages/Error";
-import { useUserMainData } from "./services/useUserMainData";
 
 const Routing = () => {
-  const userId = import.meta.env.VITE_USER_ID;
-  const usersData = useUserMainData();
-  const user = usersData?.find((userData) => userData.id === Number(userId));
-
   return (
     <Routes>
-      {user?.id && <Route path="/" element={<Navigate to={`/user/${user?.id}`} replace />} />}
-      <Route path="/user/:id" element={<Dashboard />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/*" element={<Error />} />
     </Routes>
   );
