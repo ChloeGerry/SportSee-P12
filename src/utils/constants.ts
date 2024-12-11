b32544cfc657031545b6a1395e9a87312d489416
Chloé Gerry <cgerry@corp.free.fr>
2024-12-11 16:12:22 +0100

    feat: add colors const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,9 @@
 export const KILOCALORY = "kCal";
 export const GRAMME = "g";
+export const colors = {
+  red: "#E60000",
+  extraLightGrey: "#e0e0e0",
+  lightGrey: "#9B9EAC",
+  grey: "#74798C",
+  darkGrey: "#282D30",
+};
