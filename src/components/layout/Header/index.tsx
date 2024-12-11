177adc37f3580228adb15a1698b69ce05537b285
Chloé Gerry <cgerry@corp.free.fr>
2024-12-11 16:02:18 +0100

    style: add shadow to header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -4,7 +4,7 @@ import { mainNavigationPages } from "@/data/mainNavigationPages";
 
 const Header = () => {
   return (
-    <header className="bg-light-black px-6 py-4 flex items-center">
+    <header className="bg-light-black px-6 py-4 flex items-center shadow-extra-small">
       <Logo />
       <nav className="text-2xl font-medium flex w-full justify-around">
         {mainNavigationPages.map(({ href, pageName }) => (
