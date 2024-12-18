fb9014941d16aae965ee1876331fb2788c314b0e
Chloé Gerry <cgerry@corp.free.fr>
2024-12-18 17:55:10 +0100

    feat: add mocked user enabled env variable

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const USER_ID = import.meta.env.VITE_USER_ID;
+export const USER_ID: string = import.meta.env.VITE_USER_ID;
+export const MOCKED_DATA_ENABLED: boolean = import.meta.env.VITE_MOCKED_DATA_ENABLED === "true";
