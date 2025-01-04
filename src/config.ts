3cddf39b7e90efc517eebe29acd8aa0da55a0ee1
Chloé Gerry <cgerry@corp.free.fr>
2025-01-04 18:19:44 +0100

    feat: add host env variable

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const USER_ID: string = import.meta.env.VITE_USER_ID;
 export const MOCKED_DATA_ENABLED: boolean = import.meta.env.VITE_MOCKED_DATA_ENABLED === "true";
+export const BASE_URL: string = import.meta.env.VITE_BASE_URL;
